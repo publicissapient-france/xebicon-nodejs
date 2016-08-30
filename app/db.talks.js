2940245c9658eb8cbd6332ad61e54e9cfe29fdf2
jsmadja <julien.smadja@gmail.com>
2016-08-30 13:30:18 +0200

    Data : Slots Xebicon dans un fichier JSON

--- app/db.talks.js
+++ app/db.talks.js
@@ -1,16 +1,13 @@
 const Promise = require('bluebird');
 
+const slots = require('./slots.json');
+
 const talks = {};
 
 talks.list = () =>
     new Promise(resolve => {
         setTimeout(() => {
-            resolve([
-                {title: 'Node.js comment faire un projet dans les règle de l\'art ?', type: 'craft'},
-                {title: 'Plan de bataille pour devenir une entreprise Data Driven', type: 'data'},
-                {title: 'React Native + Flow, une complémentarité hors du commun', type: 'front'},
-                {title: 'Advanced querying with Elastic Search', type: 'back'}
-            ]);
+            resolve(slots);
         }, 2000);
     });
 
