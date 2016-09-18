a5075af89f08e5b10c78180f93595d6f6f842d04
Benjamin LACROIX <benjlacroix@gmail.com>
2016-09-18 14:21:41 +0200

    region and live template

--- app/index.js
+++ app/index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// region talk service
+
+const TalkService = require('./service.talk');
+
+// endregion
+
+// region API /talks
+
+// app.get('/talks', (req, res) => TalkService.findByType(req.query.type).then(results => res.status(200).json(results)));
+
+// endregion
+
+app.listen(port, () => console.log(`Xebicon/NodeJS running on port ${port}`));
+
+module.exports = app;
--- test/integration-test.js
+++ test/integration-test.js
@@ -1,11 +1,18 @@
-/*
 'use strict';
 
+// region app
 const app = require('../app/index');
 const request = require('supertest')(app);
+// endregion
+
+// region db
 const TalkDb = require('../app/db.talk');
+// endregion
+
+// region expect and sinon
 const sinon = require('sinon');
 const expect = require('chai').expect;
+// endregion
 
 describe('Integration Tests', () => {
 
@@ -17,12 +24,25 @@ describe('Integration Tests', () => {
 
     afterEach(() => sandbox.restore());
 
-    it('should GET / and return slots', (done) => {
+    it('should GET / and return slots', done => {
         sandbox.stub(TalkDb, "list").returns(Promise.resolve([
             {title: 'Plan de bataille pour devenir une entreprise Data Driven', type: 'data'},
             {title: 'React Native + Flow, une complémentarité hors du commun', type: 'front'}
         ]));
-        // INSERT CODE HERE
+        // region request
+        // request
+        //     .get('/talks?type=data')
+        //     .expect('Content-Type', /json/)
+        //     .expect(200)
+        //     .end((err, response) => {
+        //         if (err) {
+        //             return done(err);
+        //         }
+        //         console.log(response.body);
+        //         expect(response.body).to.have.length(1);
+        //         done();
+        //     });
+        // endregion
+        // TODO request API
     });
 });
-*/
