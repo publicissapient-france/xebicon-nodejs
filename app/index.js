def5b19eb93f98469ff78b9e8aaf3838a2f29b9e
jsmadja <julien.smadja@gmail.com>
2016-09-18 14:42:37 +0200

    Remove region code ->>> Live Templates

--- app/index.js
+++ app/index.js
@@ -1,33 +1,16 @@
 'use strict';
 
 // region require
-
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
 // endregion
 
 // region talk service
-
-// const TalkService = require('./service.talk');
-
 // endregion
 
 // region API /talks
-
-// app.get('/talks', (req, res) => TalkService.findByType(req.query.type).then(results => res.status(200).json(results)));
-
 // endregion
 
 // region listen
-
-// app.listen(port, () => console.log(`Xebicon/NodeJS running on port ${port}`));
-
 // endregion
 
 // region exports
-
-// module.exports = app;
-
 // endregion
--- test/integration-test.js
+++ test/integration-test.js
@@ -1,17 +1,12 @@
 'use strict';
 
 // region app
-const app = require('../app/index');
-const request = require('supertest')(app);
 // endregion
 
 // region db
-const TalkDb = require('../app/db.talk');
 // endregion
 
 // region expect and sinon
-const sinon = require('sinon');
-const expect = require('chai').expect;
 // endregion
 
 describe('Integration Tests', () => {
@@ -30,18 +25,6 @@ describe('Integration Tests', () => {
             {title: 'React Native + Flow, une complémentarité hors du commun', type: 'front'}
         ]));
         // region request
-        // request
-        //     .get('/talks?type=data')
-        //     .expect('Content-Type', /json/)
-        //     .expect(200)
-        //     .end((err, response) => {
-        //         if (err) {
-        //             return done(err);
-        //         }
-        //         console.log(response.body);
-        //         expect(response.body).to.have.length(1);
-        //         done();
-        //     });
         // endregion
         // TODO request API
     });
