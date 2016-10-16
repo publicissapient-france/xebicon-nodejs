d702d08cf3aec3beffe7f142b9356e4b24d7b238
jsmadja <julien.smadja@gmail.com>
2016-10-16 17:47:13 +0200

    Change test name

--- test/integration-test.js
+++ test/integration-test.js
@@ -19,7 +19,7 @@ describe('Integration Tests', () => {
 
     afterEach(() => sandbox.restore());
 
-    it('should GET / and return slots', done => {
+    it('should GET /talks and return slots', done => {
         sandbox.stub(TalkDb, "list").returns(Promise.resolve([
             {title: 'Plan de bataille pour devenir une entreprise Data Driven', type: 'data'},
             {title: 'React Native + Flow, une complémentarité hors du commun', type: 'front'}
